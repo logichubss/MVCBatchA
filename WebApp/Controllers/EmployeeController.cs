c12384a963891c2bb2d2e52b93dab1b40236d7f4
logichubss <52495838+logichubss@users.noreply.github.com>
2023-02-02 14:55:16 +0530

    TempData

--- WebApp/Controllers/EmployeeController.cs
+++ WebApp/Controllers/EmployeeController.cs
@@ -6,39 +6,31 @@ namespace WebApp.Controllers
     public class EmployeeController : Controller
     {
         // action method
-        public ViewResult Index()
+        public IActionResult Index()
         {
-            List<Employee> myEmployees = new List<Employee>()
-            {
-               new Employee{ Id=101,Name="Adil",Email="adil@logichubss.com",Salary=50000},
-               new Employee{ Id=102,Name="Rizwan",Email="Rizwan@logichubss.com",Salary=50000},
-               new Employee{ Id=103,Name="Mehran",Email="Mehran@logichubss.com",Salary=50000},
-               new Employee{ Id=104,Name="Aqib",Email="Aqib@logichubss.com",Salary=50000},
-               new Employee{ Id=105,Name="Irfan",Email="Irfan@logichubss.com",Salary=50000},
-            };
-
-
-
-            List<string> players = new List<string>();
-            players.Add("hazim");
-            players.Add("abid");
-            players.Add("aqib");
-            players.Add("aqib");
-            players.Add("aqib");
-
-            ViewBag.Welcome="Welcome to logichub";
-            ViewBag.Salary=80000;
-            ViewBag.Players=players;
-            ViewBag.Employees=myEmployees;
-
+            TempData["Id"]=1001;
+            //int id = (int)TempData["Id"];
             return View();
         }
 
 
         public ViewResult About() 
         {
-            var myview = View();
-            return myview;
+           int id=(int)TempData["Id"];
+            TempData.Keep();
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            int id = (int)TempData["Id"];
+            return View();
+        }
+
+        public IActionResult Services()
+        {
+            int id = (int)TempData["Id"];
+            return View();
         }
     }
 }
